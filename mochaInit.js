f9bc8a3b5ecdad1df4dde1c04bb8a5ae799755cb
László Kardnál <laszlo.kardinal@gmail.com>
2017-10-11 12:14:34 +0200

    Module initialization

---
+++ mochaInit.js
@@ -0,0 +1,26 @@
+const React = require("react");
+
+const Enzyme = require("enzyme");
+const Adapter = require("enzyme-adapter-react-15");
+Enzyme.configure({ adapter: new Adapter() });
+const { shallow, mount } = Enzyme;
+
+const { JSDOM } = require("jsdom");
+const { document } = new JSDOM(
+  "<!doctype html><html><body></body></html>"
+).window;
+const window = document.defaultView;
+
+const { expect } = require("chai");
+
+const sinon = require("sinon");
+
+Object.assign(global, {
+  React,
+  document,
+  window,
+  shallow,
+  mount,
+  expect,
+  sinon
+});
