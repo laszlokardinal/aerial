7aebdfa4e8ff37e2e0f372767a6fcc554de63ea5
László Kardinál <laszlo.kardinal@gmail.com>
2017-10-13 19:45:25 +0200

    Upgraded to React 16

--- mochaInit.js
+++ mochaInit.js
@@ -5,7 +5,7 @@ require("babel-register")({
 const React = require("react");
 
 const Enzyme = require("enzyme");
-const Adapter = require("enzyme-adapter-react-15");
+const Adapter = require("enzyme-adapter-react-16");
 Enzyme.configure({ adapter: new Adapter() });
 const { shallow, mount } = Enzyme;
 
